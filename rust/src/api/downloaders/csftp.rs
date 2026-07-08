ff2071f4357c0a3b52a1655e375c91845bc073d6
chids04 <guyepic39@gmail.com>
2026-07-08 12:15:02 +0100

    add thiserror dep

--- rust/src/api/downloaders/csftp.rs
+++ rust/src/api/downloaders/csftp.rs
@@ -10,8 +10,8 @@ use russh_sftp::{client::SftpSession, protocol::FileType};
 use tokio::io::{AsyncReadExt, AsyncSeekExt, AsyncWriteExt};
 
 struct Csftp {
-    client: Handle<Client>,
-    init_dir: PathBuf,
+    session: SftpSession,
+    init_dir: String,
     entries: Vec<CsftpEntry>,
 }
 
@@ -19,10 +19,15 @@ struct CsftpEntry {
     file_name: String,
     path: String,
     file_type: FileType,
+    downloaded: bool,
 }
 
 // when initiating this frontend gets a csftp client object they init,
 // the should pass a directory that you want to init the connection with
+
+// plan, we should be able to view a list of file system entries
+// should show a simple 'maybe downloaded' icon to indicate that this MAY be saved
+// files are downloaded, folders should be downloadable or allow u to navigate inside of them
 
 struct Client;
 impl client::Handler for Client {
@@ -47,96 +52,116 @@ impl client::Handler for Client {
     }
 }
 
-async fn init_session(ip_addr: &str, music_path: String) {
+async fn init_session(ip_addr: &str, music_path: &str) -> Result<Csftp, ()> {
     let config = russh::client::Config::default();
     let sh = Client {};
 
     let mut session = russh::client::connect(Arc::new(config), (ip_addr, 22), sh)
         .await
         .unwrap();
-    if session
+
+    if !session
         .authenticate_password("root", "password")
         .await
         .unwrap()
         .success()
     {
-        let channel = session.channel_open_session().await.unwrap();
-        channel.request_subsystem(true, "sftp").await.unwrap();
-        let sftp = SftpSession::new(channel.into_stream()).await.unwrap();
-        info!("current path: {:?}", sftp.canonicalize(".").await.unwrap());
+        error!("failed to authenticate sftp session");
+        return Err(());
+    }
 
-        // create dir and symlink
-        let path = "./some_kind_of_dir";
-        let symlink = "./symlink";
+    // this can fail, send to ui needs to know about this
+    let channel = session.channel_open_session().await.unwrap();
+    channel.request_subsystem(true, "sftp").await.unwrap();
+    let sftp = SftpSession::new(channel.into_stream()).await.unwrap();
 
-        sftp.create_dir(path).await.unwrap();
-        sftp.symlink(path, symlink).await.unwrap();
+    info!("current path: {:?}", sftp.canonicalize(".").await.unwrap());
 
-        info!("dir info: {:?}", sftp.metadata(path).await.unwrap());
-        info!(
-            "symlink info: {:?}",
-            sftp.symlink_metadata(path).await.unwrap()
-        );
+    // create dir and symlink
+    // let path = "./some_kind_of_dir";
+    // let symlink = "./symlink";
+    //
+    // sftp.create_dir(path).await.unwrap();
+    // sftp.symlink(path, symlink).await.unwrap();
+    //
+    // info!("dir info: {:?}", sftp.metadata(path).await.unwrap());
+    // info!(
+    //     "symlink info: {:?}",
+    //     sftp.symlink_metadata(path).await.unwrap()
+    // );
 
-        let music_dir = match sftp.read_dir(music_path).await {
-            Ok(dir) => dir,
-            Err(e) => {
-                error!("sftp: failed to read directory {path} reason: {e}");
-                return;
+    // this can fail too
+    let music_dir = match sftp.read_dir(music_path).await {
+        Ok(dir) => dir,
+        Err(e) => {
+            error!("sftp: failed to read directory {music_path} reason: {e}");
+            return Err(());
+        }
+    };
+
+    let mut init_items = Vec::new();
+
+    // scanning directory
+    for entry in music_dir {
+        info!("file in directory: {:?}", entry.file_name());
+
+        let csftp_entry = match entry.file_type() {
+            FileType::File | FileType::Dir => CsftpEntry {
+                path: entry.path(),
+                file_name: entry.file_name(),
+                file_type: entry.file_type(),
+                downloaded: false,
+            },
+            _ => {
+                info!("unsupported file type {} skipping", entry.file_name());
+                continue;
             }
         };
 
-        // scanning directory
-        for entry in music_dir {
-            info!("file in directory: {:?}", entry.file_name());
-
-            let csftp_entry = match entry.file_type() {
-                FileType::File | FileType::Dir => CsftpEntry {
-                    path: entry.path(),
-                    file_name: entry.file_name(),
-                    file_type: entry.file_type(),
-                },
-                _ => {
-                    info!("unsupported file type {} skipping", entry.file_name());
-                    return;
-                }
-            };
-        }
-
-        // interaction with i/o
-        let filename = "test_new.txt";
-        let mut file = sftp
-            .open_with_flags(
-                filename,
-                OpenFlags::CREATE | OpenFlags::TRUNCATE | OpenFlags::WRITE | OpenFlags::READ,
-            )
-            .await
-            .unwrap();
-        info!("metadata by handle: {:?}", file.metadata().await.unwrap());
-
-        file.write_all(b"magic text").await.unwrap();
-        info!("flush: {:?}", file.flush().await); // or file.sync_all()
-        info!(
-            "current cursor position: {:?}",
-            file.stream_position().await
-        );
-
-        let mut str = String::new();
-
-        file.rewind().await.unwrap();
-        file.read_to_string(&mut str).await.unwrap();
-        file.rewind().await.unwrap();
-
-        info!(
-            "our magical contents: {}, after rewind: {:?}",
-            str,
-            file.stream_position().await
-        );
-
-        file.shutdown().await.unwrap();
-        sftp.remove_file(filename).await.unwrap();
-
-        // should fail because handle was closed
-        error!("should fail: {:?}", file.read_u8().await);
+        init_items.push(csftp_entry);
     }
+
+    let csftp = Csftp {
+        session: sftp,
+        init_dir: music_path.to_string(),
+        entries: init_items,
+    };
+
+    Ok(csftp)
+
+    // interaction with i/o
+    // let filename = "test_new.txt";
+    // let mut file = sftp
+    //     .open_with_flags(
+    //         filename,
+    //         OpenFlags::CREATE | OpenFlags::TRUNCATE | OpenFlags::WRITE | OpenFlags::READ,
+    //     )
+    //     .await
+    //     .unwrap();
+    // info!("metadata by handle: {:?}", file.metadata().await.unwrap());
+    //
+    // file.write_all(b"magic text").await.unwrap();
+    // info!("flush: {:?}", file.flush().await); // or file.sync_all()
+    // info!(
+    //     "current cursor position: {:?}",
+    //     file.stream_position().await
+    // );
+    //
+    // let mut str = String::new();
+    //
+    // file.rewind().await.unwrap();
+    // file.read_to_string(&mut str).await.unwrap();
+    // file.rewind().await.unwrap();
+    //
+    // info!(
+    //     "our magical contents: {}, after rewind: {:?}",
+    //     str,
+    //     file.stream_position().await
+    // );
+    //
+    // file.shutdown().await.unwrap();
+    // sftp.remove_file(filename).await.unwrap();
+    //
+    // // should fail because handle was closed
+    // error!("should fail: {:?}", file.read_u8().await);
 }
